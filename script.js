5fb19170f4ab00cffa56f29aa59a96f84d4608aa
somaiaGholami <145608582+somaiaGholami@users.noreply.github.com>
2024-05-29 23:09:23 +0430

    Add files via upload

---
+++ script.js
@@ -0,0 +1,49 @@
+
+ /* menu part */
+
+ const list = document.querySelectorAll('.list');
+ function activelink(){
+     list.forEach((item) => 
+     item.classList.remove('active'));
+     this.classList.add('active')
+ }
+
+ list.forEach((item) => 
+ item.addEventListener('click',activelink));
+
+
+ /* project part */
+ let next = document.querySelector('.next');
+ let prev = document.querySelector('.prev');
+ let slider = document.querySelector('.slider');
+
+
+ next.addEventListener('click', function(){
+     let slides = document.querySelectorAll('.slides');
+     slider.appendChild(slides[0]);
+ })
+
+ prev.addEventListener('click', function(){
+     let slides = document.querySelectorAll('.slides');
+     slider.prepend(slides[5]);
+ })
+
+
+
+
+ /* graphic design project part */
+ let nextG = document.querySelector('.next-gr');
+ let prevG = document.querySelector('.prev-gr');
+ let sliderG = document.querySelector('.slider-gr');
+
+
+ nextG.addEventListener('click', function(){
+     let slidesG = document.querySelectorAll('.slides-gr');
+     sliderG.appendChild(slidesG[0]);
+ })
+
+ prevG.addEventListener('click', function(){
+     let slidesG = document.querySelectorAll('.slides-gr');
+     sliderG.prepend(slidesG[16]);
+ })
+
